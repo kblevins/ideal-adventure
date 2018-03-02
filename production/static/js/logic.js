1ce2ce44e46e55d185d603e17e11b8e1d8063a19
ars0107 <arwen.ricard@gmail.com>
2018-03-02 10:50:19 -0600

    styling and fixing for the webpages

--- production/static/js/logic.js
+++ production/static/js/logic.js
@@ -1,8 +1,8 @@
 // needs to take an argument for the state to center on that state
 // and set the zoom
 var birdIcon = L.icon({
-  iconUrl: '../static/images/solid bird.png',
-  shadowUrl: '../static/images/bird shadow.png',
+  iconUrl: '/static/images/solid bird.png',
+  shadowUrl: '/static/images/bird shadow.png',
 
   iconSize:     [32, 22], // size of the icon
   shadowSize:   [44, 10], // size of the shadow
@@ -17,7 +17,7 @@ function stateMap(ST){
   Plotly.d3.json(url, function(error, data){
     if (error) return console.warn(error);
 
-    document.getElementById('stateMap').innerHTML = "<div id='map' style='width: 600px; height: 450px; margin: 10px auto;'></div>";
+    document.getElementById('stateMap').innerHTML = "<div id='map'></div>";
     var osmUrl = "https://api.mapbox.com/styles/v1/kkblevins/cje0f0el638792rmw5y3yw648/tiles/256/{z}/{x}/{y}?"+
     "access_token=pk.eyJ1Ijoia2tibGV2aW5zIiwiYSI6ImNqZGhqeWlxaDBiZ2kydnNhYTlseDE3eTYifQ.EWlCoyNVcod37iJ0nUdG3w"
     osmLayer = new L.TileLayer(osmUrl);
@@ -42,7 +42,7 @@ function RegionMap(Region){
   Plotly.d3.json(url, function(error, data){
     if (error) return console.warn(error);
 
-    document.getElementById('regionMap').innerHTML = "<div id='map' style='width: 600px; height: 500px;'></div>";
+    document.getElementById('regionMap').innerHTML = "<div id='map'></div>";
     var osmUrl = "https://api.mapbox.com/styles/v1/kkblevins/cje0f0el638792rmw5y3yw648/tiles/256/{z}/{x}/{y}?"+
     "access_token=pk.eyJ1Ijoia2tibGV2aW5zIiwiYSI6ImNqZGhqeWlxaDBiZ2kydnNhYTlseDE3eTYifQ.EWlCoyNVcod37iJ0nUdG3w"
     osmLayer = new L.TileLayer(osmUrl);
